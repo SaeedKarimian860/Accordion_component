c92a856de4359a499a140048becdff226db98596
SaeedKarimian860 <SGK12190@gmail.com>
2025-03-28 20:40:48 -0400

    Accordion Component

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function AccordionItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
